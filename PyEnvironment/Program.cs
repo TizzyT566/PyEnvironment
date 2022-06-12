d34558eaf4925d744ab0556e70c383d75f0a4edb
Tien Hyun <tizzyt566@gmail.com>
2022-06-12 18:02:50 -0400

    Add project files.

---
+++ PyEnvironment/Program.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PyEnvironment;
+using System.Collections.Concurrent;
+
+DiscordSocketConfig config = new()
+{
+    AlwaysDownloadUsers = true,
+    GatewayIntents = GatewayIntents.AllUnprivileged ^ GatewayIntents.GuildScheduledEvents ^ GatewayIntents.GuildInvites,
+    MessageCacheSize = 1000
+};
+
+DiscordSocketClient _client;
+
+ConcurrentDictionary<ulong, PyInstance> instances = new();
+
+_client = new DiscordSocketClient(config);
+
+await _client.LoginAsync(TokenType.Bot, args[0]);
+
+_client.Log += Log;
+
+_client.MessageReceived += async msg =>
+{
+    if (msg.Author.IsBot || msg.Channel.Name != "python") return;
+
+    PyInstance node = instances.GetOrAdd(msg.Channel.Id, id => new(async output => await msg.Channel.SendMessageAsync(output)));
+
+    if (msg.Content.StartsWith("```py") && msg.Content.EndsWith("```"))
+        await node.WriteAsync(msg.Content[5..^3]);
+};
+
+await _client.StartAsync();
+
+await Task.Delay(-1);
+
+static Task Log(LogMessage msg)
+{
+    Console.WriteLine(msg.ToString());
+    return Task.CompletedTask;
+}
---
+++ PyEnvironment/PyInstance.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace PyEnvironment
+{
+    public class PyInstance
+    {
+        private readonly Process _python;
+
+        public PyInstance(Action<string> output, Action<string>? error = null, int ignoreInitialLines = 2)
+        {
+            if (output is null) throw new ArgumentNullException(nameof(output));
+
+            _python = new();
+            _python.StartInfo.FileName = "python";
+            _python.StartInfo.Arguments = "-i";
+            _python.StartInfo.RedirectStandardInput = true;
+            _python.StartInfo.RedirectStandardOutput = true;
+            _python.StartInfo.RedirectStandardError = true;
+            _python.StartInfo.UseShellExecute = false;
+
+            int outputLines = ignoreInitialLines;
+            int errorLines = ignoreInitialLines;
+
+            _python.OutputDataReceived += (sender, e) =>
+            {
+                if (outputLines > 0) outputLines--;
+                else if (e.Data is not null && e.Data.Trim() != "" && e.Data.Trim() != "undefined")
+                {
+                    if (e.Data.StartsWith(">>> "))
+                        output.Invoke(e.Data[4..]);
+                    else
+                        output.Invoke(e.Data);
+                }
+            };
+            _python.ErrorDataReceived += (sender, e) =>
+            {
+                if (errorLines > 0) errorLines--;
+                else if (error is not null && e.Data is not null && e.Data.Trim() != "" && e.Data.Trim() != "undefined")
+                {
+                    if (e.Data.StartsWith(">>> "))
+                        error.Invoke(e.Data[4..]);
+                    else
+                        error.Invoke(e.Data);
+                }
+            };
+            _python.Start();
+            _python.BeginOutputReadLine();
+            _python.BeginErrorReadLine();
+        }
+
+        public Task WaitForExitAsync() => _python.WaitForExitAsync();
+
+        public Task WriteAsync(string content) => _python.StandardInput.WriteAsync(content);
+    }
+}
